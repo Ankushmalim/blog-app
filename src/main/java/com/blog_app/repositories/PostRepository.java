a4b36887b55a3f7d4c7a63b42cb654776660e82f
ankush <ankush@gmail.com>
2025-12-31 03:38:14 +0530

    add validation for check already post available

--- src/main/java/com/blog_app/repositories/PostRepository.java
+++ src/main/java/com/blog_app/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package com.blog_app.repositories;
 
 import com.blog_app.entities.Post;
 import com.blog_app.enums.PostStatus;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findByStatus(PostStatus status, Pageable pageable);
 
     Page<Post> findByUserId(Long userId, Pageable pageable);
+
+    boolean existsByTitle(String title);
 }
